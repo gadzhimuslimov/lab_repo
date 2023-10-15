c56109687ec1a65f2cb71fea9231f6b0f04b160e
Ragim <147828799+gadzhimuslimov@users.noreply.github.com>
2023-10-15 22:29:35 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,4 +1,10 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-memory_disk = 1024 ** 2 * 1.44
-memory_book = 4 * 25 * 50 * 100
-print("Количество книг, помещающихся на дискету:", int(memory_disk // memory_book))
+memory_disk = 1024 ** 2 * 1.44  # Переводим объем памяти в байты
+pages = 100
+rows = 50
+symbols = 25
+memory_symbol = 4
+memory_book = memory_symbol * symbols * rows * pages
+
+count_book = int(memory_disk // memory_book)
+print("Количество книг, помещающихся на дискету:", count_book)
