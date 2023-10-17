ae65d2a864d213211cb5aaf99d162041c53a1e98
Ragim <147828799+gadzhimuslimov@users.noreply.github.com>
2023-10-17 14:46:16 +0300

    Update task_3.py

--- Лабораторная работа 1/task_3.py
+++ Лабораторная работа 1/task_3.py
@@ -1,7 +1,7 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
 # TODO Разделите участников на две команды
-equally = int(len(list_players) / 2)
+equally = len(list_players) // 2
 team1 = list_players[:equally]
 team2 = list_players[equally:]
 
